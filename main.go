044b628bb841761c43b0c0a2ecd166a10e4d8aa6
Young hoon Go <kyhsa93@naver.com>
2020-12-05 16:06:57 +0900

    refactor: print log when app started

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	_ "github/four-servings/meonzi/accounts"
+	"log"
 	"net/http"
 )
 
 func main() {
+	log.Println("app is listening on port 5000")
 	http.ListenAndServe(":5000", nil)
 }
