c8bc50d0df5a83cbaea97857cd24d41fb7843e5a
Lee Jaeseong <fg9658@gmail.com>
2021-02-27 11:20:42 +0900

    feat(account-query): 소셜 로그인 가입정보를 찾는 쿼리

--- account/query/query.go
+++ account/query/query.go
@@ -1,1 +1,14 @@
 package query
+
+import (
+	"context"
+	"github/four-servings/meonzi/ent/schema"
+)
+
+type (
+	GetAccountBySocial struct {
+		Ctx context.Context
+		SocialType schema.SocialType
+		SocialId string
+	}
+)
