805b794361a905fa4b1f5d3f6b0704cf4998b468
Lee Jaeseong <fg9658@gmail.com>
2021-02-27 11:17:15 +0900

    feat: 로컬 bus(게이트웨이), pubsub(큐는 존재 x) 추가

--- local/bus.go
+++ local/bus.go
@@ -4,7 +4,13 @@ import (
 	"context"
 	"errors"
 	"reflect"
+	"sync"
 	"time"
+)
+
+var (
+	contextType = reflect.TypeOf(new(context.Context)).Elem()
+	errorType = reflect.TypeOf(new(error)).Elem()
 )
 
 type BusHandlerFunc func(ctx context.Context, value interface{}) error
@@ -14,44 +20,126 @@ type BusHandler interface {
 }
 
 type Bus interface {
-	Registry(value interface{}, handler BusHandler) error
+	RegistryStrictHandler(targetType interface{}, handler BusHandler) error
+	RegistryHandler(targetType interface{}, handler interface{}) error
 	Execute(value interface{}) error
 	ExecuteContext(ctx context.Context, value interface{}) error
 }
 
 type busImpl struct {
+	lock sync.RWMutex
 	gateway map[reflect.Type]BusHandlerFunc
-	timeout time.Duration
+	timeout *time.Duration
 }
 
-func NewBus(timeout time.Duration) Bus {
+func NewBus() Bus {
 	return &busImpl{
 		gateway: make(map[reflect.Type]BusHandlerFunc),
-		timeout: timeout,
 	}
 }
 
-func (b *busImpl) Registry(value interface{}, handler BusHandler) error {
-	typ := reflect.TypeOf(value)
-	_, ok := b.gateway[typ]
-	if ok {
+func NewBusWithTimeout(timeout time.Duration) Bus {
+	return &busImpl{
+		gateway: make(map[reflect.Type]BusHandlerFunc),
+		timeout: &timeout,
+	}
+}
+
+func (b *busImpl) RegistryHandler(targetType interface{}, handler interface{}) error {
+	typ := reflect.TypeOf(targetType)
+	if b.exists(typ) {
 		return errors.New("already registry value")
 	}
-	b.gateway[typ] = handler.Wrap()
+
+	action := reflect.ValueOf(handler)
+	actionType := action.Type()
+	inCount := actionType.NumIn()
+	outCount := actionType.NumOut()
+
+	if inCount > 2 {
+		return errors.New("handler call arguments count overflow")
+	} else if inCount == 2 && (contextType != actionType.In(0) || typ != actionType.In(1)) {
+		//TODO error message
+		return errors.New("")
+	} else if inCount == 1 && typ != actionType.In(0) {
+		//TODO error message
+		return errors.New("")
+	}
+
+	if outCount > 1 {
+		return errors.New("handler call returns count overflow")
+	} else if outCount == 1 && !actionType.Out(0).Implements(errorType) {
+		return errors.New("handler call return must be error")
+	}
+
+	b.set(typ, func(ctx context.Context, value interface{}) error {
+		in := make([]reflect.Value, inCount)
+
+		switch inCount {
+		case 2:
+			in[0] = reflect.ValueOf(ctx)
+			in[1] = reflect.ValueOf(value)
+		case 1:
+			in[0] = reflect.ValueOf(value)
+		}
+
+		out := action.Call(in)
+		if outCount > 0 {
+			return out[0].Interface().(error)
+		} else {
+			return nil
+		}
+	})
+	return nil
+}
+
+func (b *busImpl) RegistryStrictHandler(targetType interface{}, handler BusHandler) error {
+	typ := reflect.TypeOf(targetType)
+	if b.exists(typ) {
+		return errors.New("already registry value")
+	}
+
+	b.set(typ, handler.Wrap())
 	return nil
 }
 
 func (b *busImpl) Execute(value interface{}) error {
-	c, cancel := context.WithTimeout(context.Background(), b.timeout)
-	defer cancel()
-	return b.ExecuteContext(c, value)
+	return b.ExecuteContext(context.Background(), value)
 }
 
 func (b *busImpl) ExecuteContext(ctx context.Context, value interface{}) error {
-	typ := reflect.TypeOf(value)
-	handle, ok := b.gateway[typ]
-	if ok {
-		return errors.New("")
+	c := ctx
+	if b.timeout != nil {
+		var cancel context.CancelFunc
+		c, cancel = context.WithTimeout(ctx, *b.timeout)
+		defer cancel()
 	}
-	return handle(ctx, value)
+
+	typ := reflect.TypeOf(value)
+	handle, ok := b.get(typ)
+	if !ok {
+		return errors.New("handler not exists")
+	}
+	return handle(c, value)
+}
+
+// critical section method
+func (b *busImpl) set(typ reflect.Type, handler BusHandlerFunc) {
+	b.lock.Lock()
+	defer b.lock.Unlock()
+	b.gateway[typ] = handler
+}
+
+func (b *busImpl) get(typ reflect.Type) (handler BusHandlerFunc, ok bool) {
+	b.lock.RLock()
+	defer b.lock.RUnlock()
+	handler, ok = b.gateway[typ]
+	return
+}
+
+func (b *busImpl) exists(typ reflect.Type) (ok bool) {
+	b.lock.RLock()
+	defer b.lock.RUnlock()
+	_, ok = b.gateway[typ]
+	return
 }
